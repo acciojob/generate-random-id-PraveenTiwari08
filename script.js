e47e49b51a70bbfb0aea87955f1c0aad76e3997b
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-05-04 20:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	 const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < l; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.l));
+    }
+    return result;
 }
-
+ 
 // Do not change the code below.
-// const l = prompt("Enter a number.");
-// alert(makeid(l));
+const l = prompt("Enter a number.");
+alert(makeid(l));
